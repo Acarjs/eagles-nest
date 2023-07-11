71929339a7cc20cc7506365e7210ac1594927c64
Anil Acar <ostravba@gmail.com>
2023-07-11 11:19:42 +0200

    from netlify function

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import PageSection from '../components/PageSection.jsx'
+
+const CheckoutPage = () => {
+  return (
+    <main>
+      <PageSection title="checkout" />
+      <Wrapper className="page">
+        <h1>Checkout will be here</h1>
+      </Wrapper>
+    </main>
+  )
+}
+
+const Wrapper = styled.div``
+
+export default CheckoutPage
